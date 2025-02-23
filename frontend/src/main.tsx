19661d5042d3707f687166dc530750a02530a3f1
Ayomide Hakeem <ayomidesuleimanh@gmail.com>
2025-02-22 21:48:25 -0800

    fixed rrrors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <BrowserRouter>
       <UserProvider>
-        <App />
-      </UserProvider>
+    <App />
+    </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
