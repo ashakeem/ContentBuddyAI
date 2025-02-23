217604cd1fe1e451421153453f4c3f10d41ab08b
Ayomide Hakeem <ayomidesuleimanh@gmail.com>
2025-02-22 21:45:40 -0800

    updated config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <UserProvider>
-    <App />
-    </UserProvider>
-    </BrowserRouter>  
-  </StrictMode>,
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,18 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    port: 3000,
-    strictPort: true,
+    middlewareMode: false,
     headers: {
-      "Content-Type": "application/javascript",
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+      'Content-Type': 'application/javascript',
     },
   },
 })
